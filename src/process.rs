3c543e323c3a93e7ca413257083bb01529042d97
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2025-09-05 10:57:25 +0200

    Allow the daemonbase::process::Process::Config struct fields to be set other than via Clap args or a ConfigFile object.

--- src/process.rs
+++ src/process.rs
@@ -467,6 +467,31 @@ mod unix {
                 self.group = Some(group)
             }
         }
+
+        pub fn with_pid_file(mut self, v: ConfigPath) -> Self {
+            self.pid_file = Some(v);
+            self
+        }
+
+        pub fn with_working_dir(mut self, v: ConfigPath) -> Self {
+            self.working_dir = Some(v);
+            self
+        }
+
+        pub fn with_chroot(mut self, v: ConfigPath) -> Self {
+            self.chroot = Some(v);
+            self
+        }
+
+        pub fn with_user(mut self, v: &str) -> Result<Self, String> {
+            self.user = Some(UserId::from_str(v)?);
+            Ok(self)
+        }
+
+        pub fn with_group(mut self, v: &str) -> Result<Self, String> {
+            self.group = Some(GroupId::from_str(v)?);
+            Ok(self)
+        }
     }
 
 
@@ -705,6 +730,26 @@ mod noop {
         /// Applies the arguments to the process.
         pub fn apply_args(&mut self, args: Args) {
             let _ = args;
+        }
+
+        pub fn with_pid_file(mut self, _: ConfigPath) -> Self {
+            self
+        }
+
+        pub fn with_working_dir(mut self, _: ConfigPath) -> Self {
+            self
+        }
+
+        pub fn with_chroot(mut self, _: ConfigPath) -> Self {
+            self
+        }
+
+        pub fn with_user(mut self, _: &str) -> Result<Self, String> {
+            Ok(self)
+        }
+
+        pub fn with_group(mut self, _: &str) -> Result<Self, String> {
+            Ok(self)
         }
     }
 
