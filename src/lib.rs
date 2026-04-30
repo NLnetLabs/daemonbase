550974e0e1b9d6507d474aac71f6b66da34a0573
Martin Hoffmann <martin@nlnetlabs.nl>
2026-04-30 14:46:44 +0200

    Add a top-level description in 'lib.rs'

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! Foundational utilities for system daemons.
+//!
+//! Daemons (as they are known on Unix-based systems) are background processes
+//! that provide continuous services, e.g. monitoring the system, playing
+//! music, and serving websites. This class of programs have many shared
+//! concerns: they usually load configuration files, interact with system
+//! privileges, and log events. `daemonbase` offers simple but opinionated
+//! solutions to these needs.
+//
+// TODO: Document usage.
+// TODO: Document portability.
+//
 pub mod config;
 pub mod error;
 pub mod logging;
